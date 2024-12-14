872ce2ba4db72769dbe5ba34887a4e0e0fe3867b
gisel <gsb7@aluno.ifal.edu.br>
2024-12-13 21:01:55 -0300

    Atualizacao da classe vestido

--- lib/bd/vestido_dao.dart
+++ lib/bd/vestido_dao.dart
@@ -1,8 +1,9 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:untitled1/bd/db_helper.dart';
-import 'package:untitled1/domain/catalogodoopcoesvestidos.dart'; // Importando o modelo
+import 'package:untitled1/domain/catalogodoopcoesvestidos.dart';
 
 class VestidoDao {
+  // Listar vestidos
   Future<List<Catalogodoopcoesvestidos>> listarVestidos() async {
     Database database = await DBHelper().initDB();
     String sql = 'SELECT * FROM VESTIDOS;';
@@ -16,42 +17,13 @@ class VestidoDao {
     return lista;
   }
 
-  Future<List<Catalogodoopcoesvestidos>> listarRoupas() async {
+  // Salvar vestido
+  Future<void> saveVestido(Catalogodoopcoesvestidos vestido) async {
     Database database = await DBHelper().initDB();
-    String sql = 'SELECT * FROM ROUPAS;';
-    var result = await database.rawQuery(sql);
-
-    List<Catalogodoopcoesvestidos> lista = [];
-    for (var json in result) {
-      Catalogodoopcoesvestidos roupa = Catalogodoopcoesvestidos.fromJson(json);
-      lista.add(roupa);
-    }
-    return lista;
-  }
-
-  Future<void> inserirVestido(Catalogodoopcoesvestidos vestido) async {
-    Database database = await DBHelper().initDB();
-    String sql = '''
-      INSERT INTO VESTIDOS (url_image, titulo, preco) 
-      VALUES (?, ?, ?);
-    ''';
-    await database.rawInsert(sql, [vestido.urlImage, vestido.titulo, vestido.preco]);
-  }
-
-  Future<void> atualizarVestido(Catalogodoopcoesvestidos vestido) async {
-    Database database = await DBHelper().initDB();
-    String sql = '''
-      UPDATE VESTIDOS 
-      SET url_image = ?, titulo = ?, preco = ? 
-      WHERE url_image = ? AND titulo = ?;
-    ''';
-    await database.rawUpdate(
-        sql, [vestido.urlImage, vestido.titulo, vestido.preco, vestido.urlImage, vestido.titulo]);
-  }
-
-  Future<void> deletarVestido(Catalogodoopcoesvestidos vestido) async {
-    Database database = await DBHelper().initDB();
-    String sql = 'DELETE FROM VESTIDOS WHERE url_image = ? AND titulo = ?;';
-    await database.rawDelete(sql, [vestido.urlImage, vestido.titulo]);
+    await database.insert(
+      'VESTIDOS',
+      vestido.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
   }
 }
