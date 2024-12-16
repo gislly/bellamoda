9e940c59d630dd80ba595792a1e1174504af801a
Aluno <eal3@aluno.ifal.edu.br>
2024-12-16 09:51:59 -0300

    mudança main

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,13 @@ import 'package:untitled1/detail_page.dart';
 import 'package:untitled1/login.dart';
 import 'package:untitled1/carrinho_page.dart';
 import 'package:flutter/material.dart';
-
+import 'package:untitled1/tela_page.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-        home: SegundaPagina()
+        home: TelaPage()
     ),
   );
 }
